964b7df51d6009c54a80591ad5e32b5975efb158
henry stamerjohann <headmin@me.com>
2011-11-30 22:30:37 +0100

    comments added
    additional comments

---
+++ recurse_dirs.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+##description    :scans a directory recursively, display  
+##author         :henry stamerjohann 
+##date           :2011-07-24
+##version        :0.4 
+
+
+# function to scan dir recursively, 
+# variable $1 substituted will be the root directory
+recurse() {
+ for i in "$1"/*;do
+    if [ -d "$i" ];then
+        echo "dir: $i"
+        recurse "$i"
+    elif [ -f "$i" ]; then
+        echo "file: $i"
+    fi
+ done
+}
+
+# function to list only filenames without path
+file_remove_paths() {
+grep file | sed 's/.*\///'
+}
+
+# function to list only the directories without path
+dir_remove_paths() {
+grep dir | sed 's/.*\///'
+}
+
+## function call, substitute path | pipe function to strip path component > display font names
+echo "files in /Library/Fonts/ "
+echo ""
+recurse /Library/Fonts/ | file_remove_paths
+echo ""
+
+## function call, substitute path | pipe function to strip path component > display dir names
+echo "dirs in /Library/Fonts/"
+echo ""
+recurse /Library/Fonts/ | dir_remove_paths 
+echo ""
+
+exit 0
